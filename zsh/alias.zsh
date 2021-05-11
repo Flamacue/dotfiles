292ae6c55161c5323cc28029fcd9c276bef7f930
flamacue <asumner@fubo.tv>
2021-05-11 12:33:16 -0400

    add git local branch cleanup alias

--- zsh/alias.zsh
+++ zsh/alias.zsh
@@ -8,3 +8,6 @@ alias k=kubectl
 # Python
 alias python=`which python3`                                                                                                                                                                                       
 alias pip=`which pip3`  
+
+# Git
+alias git-bc="git fetch origin --prune && git branch -v | grep \"\[gone\]\" | awk '{print \$1}' | xargs git branch -D"
