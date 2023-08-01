630a2689777fbbaa540b287cb78ea8ebeaee6e38
flamacue <adam.sumner@hashicorp.com>
2023-08-01 14:44:52 -0500

    update to use pyenv

--- zsh/alias.zsh
+++ zsh/alias.zsh
@@ -5,9 +5,5 @@ alias chromium="chromium </dev/null &>/dev/null &"
 # Kubernetes
 alias k=kubectl
 
-# Python
-alias python=`which python3`                                                                                                                                                                                       
-alias pip=`which pip3`  
-
 # Git
 alias git-bc="git fetch origin --prune && git branch -v | grep \"\[gone\]\" | awk '{print \$1}' | xargs git branch -D"
