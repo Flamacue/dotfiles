0a314198b57e69256aebdfbddde4296b6b0e9594
flamacue <adam.sumner@hashicorp.com>
2026-09-04 13:22:32 -0500

    feat: add ruby export

--- zsh/export.zsh
+++ zsh/export.zsh
@@ -1,6 +1,7 @@
 # Golang
 export GOPATH=~/go                                                                                                                                                                                                 
-export PATH="$GOPATH/bin:$PATH"
+export PATH="$GOPATH/bin:/opt/homebrew/lib/ruby/gems/3.4.0/bin:$PATH"
+export PATH="/opt/homebrew/opt/ruby/bin:$PATH"
 
 # Editor
 export EDITOR=vim
